d3147fc9c69853029885cbff469fc804be5d88ca
Luis Lopez <delucholuis@gmail.com>
2018-03-26 10:14:27 -0700

    Homepage API Controller and React Sign In
    Read comments for placeholders

--- my-app/src/SignIn.jsx
+++ my-app/src/SignIn.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SignIn extends Component {
     render() {
         return(
-            <div>SignIn</div>
+            <button type="button">Sign In!</button>
         )
     }
 }
