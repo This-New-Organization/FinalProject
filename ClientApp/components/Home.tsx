8a07209e4e877a66f3afe2e9a62ca45c32b575f4
Luis Lopez <delucholuis@gmail.com>
2018-04-03 09:50:08 -0700

    Added footer, header, layout.

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
+
 export default class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
+        // Body will go on the <div></div> tag
         return <div>
             <h1>Hello, world!</h1>
             <p>Welcome to your new single-page application, built with:</p>
--- ClientApp/components/Layout.tsx
+++ ClientApp/components/Layout.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 import { NavMenu } from './NavMenu';
+import Footer from "./Footer";
+import Header from "./Header";
 
 export class Layout extends React.Component<{}, {}> {
     public render() {
         return <div className='container-fluid'>
             <div className='row'>
                 <div className='col-sm-3'>
+                {/* Navmenu and Header placement to be dicussed */}
                     <NavMenu />
+                    <Header />
                 </div>
                 <div className='col-sm-9'>
                     { this.props.children }
                 </div>
+                <Footer />
             </div>
         </div>;
     }
