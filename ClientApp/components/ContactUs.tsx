436032584db300a2d90243427305632422be6765
Unknown <sheryljudy@yahoo.com>
2018-04-06 14:27:20 -0700

    Contact Us page.

--- ClientApp/components/ContactUs.tsx
+++ ClientApp/components/ContactUs.tsx
@@ -2,10 +2,41 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 export default class ContactUs extends React.Component<RouteComponentProps<{}>, {}> {
-    public render() {
-        return <div>
-            <h1>Contact us</h1>
-            <p>This will be a page with our contacts information</p>
-        </div>;
-    }
-}
+  constructor(){
+    super();
+    this.state = {email: null, name: null, message: null};
+  }
+   submitHandler() {
+    fetch("http://localhost:5000/api/ContactUs/")
+    .then()
+      return (Response);
+  }
+
+
+  private updateEmail(event: any){
+    this.setState({
+      ...this.state, email: event.target.value
+    });
+  }
+
+  public render() {
+    return (
+      <div>
+        <h1>Contact us</h1>
+        <input type='email' onChange={this.updateEmail}/>
+        <input type='textbox' />
+        <textarea/>
+        <form action="/ContactUs">
+        First name:<br/>
+        <input type="text" name="firstname" value="First name"/>
+      <br/>
+       Last name: <br/>
+      <input type="text" name="lastname" value="Last name"/>
+      <br/>
+        <button onClick={this.submitHandler}></button>
+        <input type="submit" value="Submit"/>
+       </form>
+      </div>
+    );
+  }
+ }
--- Startup.cs
+++ Startup.cs
@@ -1,99 +1,99 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.SpaServices.Webpack;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using FinalProject.Models;
-using FinalProject.Data;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.AspNetCore.SpaServices.Webpack;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using FinalProject.Models;
+// using FinalProject.Data;
 
-namespace FinalProject
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+// namespace FinalProject
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
 
-        public IConfiguration Configuration { get; }
+//         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<CCFPContext>(
-                // Use MySql package for connection (may require restart of VSCode)
-            options => options.UseMySql(
-                    // Use appsettings.json ConnectionStrings: DefaultConnection
-                    // NOTE: You should add appsettings.json to .gitIgnore to prevent 
-                    //      settings from being visible in public repositories
-            Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc();
-        }
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
+//             services.AddDbContext<CCFPContext>(
+//                 // Use MySql package for connection (may require restart of VSCode)
+//             options => options.UseMySql(
+//                     // Use appsettings.json ConnectionStrings: DefaultConnection
+//                     // NOTE: You should add appsettings.json to .gitIgnore to prevent 
+//                     //      settings from being visible in public repositories
+//             Configuration.GetConnectionString("DefaultConnection")));
+//             services.AddMvc();
+//         }
 
-        public static void Initialize(IServiceProvider serviceProvider)
-        {
-            //Add check for pending migrations to database
-            //Try/Catch for catching when migration created causes error
-            try
-            {
-                var context = serviceProvider.GetService<CCFPContext>();
-                if (context.Database.GetPendingMigrations().Any())
-                {
-                    context.Database.Migrate();
-                }
+//         public static void Initialize(IServiceProvider serviceProvider)
+//         {
+//             //Add check for pending migrations to database
+//             //Try/Catch for catching when migration created causes error
+//             try
+//             {
+//                 var context = serviceProvider.GetService<CCFPContext>();
+//                 if (context.Database.GetPendingMigrations().Any())
+//                 {
+//                     context.Database.Migrate();
+//                 }
 
-                if (!context.TeamMembers.Any())
-                {
-                    context.TeamMembers.Add(new TeamMembers { NameFirst = "Reginald", NameLast = "Beason", About = "Reginald", Title = "Beason"});
-                    context.TeamMembers.Add(new TeamMembers { NameFirst = "Marshall", NameLast = "Frink", About = "Marshall", Title = "Frink" });                    context.TeamMembers.Add(new TeamMembers { NameFirst = "Reginald", NameLast = "Beason", About = "Reginald", Title = "Beason"});
-                    context.TeamMembers.Add(new TeamMembers { NameFirst = "Sheryl", NameLast = "Choun", About = "Sheryl", Title = "Choun" });
-                    context.TeamMembers.Add(new TeamMembers { NameFirst = "Luis", NameLast = "Lopez", About = "Luis", Title = "Lopez" });
-                    context.SaveChanges();
-                }
-            }
-            catch (Exception ex){
-                Console.WriteLine("Unable to seed database.");
-                Console.WriteLine(ex.Message);
-            }
+//                 if (!context.TeamMembers.Any())
+//                 {
+//                     context.TeamMembers.Add(new TeamMembers { NameFirst = "Reginald", NameLast = "Beason", About = "Reginald", Title = "Beason"});
+//                     context.TeamMembers.Add(new TeamMembers { NameFirst = "Marshall", NameLast = "Frink", About = "Marshall", Title = "Frink" });                    context.TeamMembers.Add(new TeamMembers { NameFirst = "Reginald", NameLast = "Beason", About = "Reginald", Title = "Beason"});
+//                     context.TeamMembers.Add(new TeamMembers { NameFirst = "Sheryl", NameLast = "Choun", About = "Sheryl", Title = "Choun" });
+//                     context.TeamMembers.Add(new TeamMembers { NameFirst = "Luis", NameLast = "Lopez", About = "Luis", Title = "Lopez" });
+//                     context.SaveChanges();
+//                 }
+//             }
+//             catch (Exception ex){
+//                 Console.WriteLine("Unable to seed database.");
+//                 Console.WriteLine(ex.Message);
+//             }
 
 
-        }
+//         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            Initialize(app.ApplicationServices);
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
-                    HotModuleReplacement = true,
-                    ReactHotModuleReplacement = true
-                });
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+//         {
+//             Initialize(app.ApplicationServices);
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseDeveloperExceptionPage();
+//                 app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+//                 {
+//                     HotModuleReplacement = true,
+//                     ReactHotModuleReplacement = true
+//                 });
+//             }
+//             else
+//             {
+//                 app.UseExceptionHandler("/Home/Error");
+//             }
 
-            app.UseStaticFiles();
+//             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+//             app.UseMvc(routes =>
+//             {
+//                 routes.MapRoute(
+//                     name: "default",
+//                     template: "{controller=Home}/{action=Index}/{id?}");
 
-                routes.MapSpaFallbackRoute(
-                    name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" });
-            });
-        }
-    }
-}
+//                 routes.MapSpaFallbackRoute(
+//                     name: "spa-fallback",
+//                     defaults: new { controller = "Home", action = "Index" });
+//             });
+//         }
+//     }
+// }
